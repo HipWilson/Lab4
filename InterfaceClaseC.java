15aa202fb82d2e435007f8cb72e2704d19ac7eed
JCova10 <ova24701@uvg.edu.gt>
2024-11-15 19:36:55 -0800

    Update InterfaceClaseC.java0

---
+++ InterfaceClaseC.java
@@ -0,0 +1,7 @@
+public class RadioClaseC implements InterfaceGeneral, InterfaceClaseC {
+    private boolean encendido = false;
+    private int volumen = 10;
+    private String banda = "FM";
+    private double frecuencia = 88.1;
+    private String[] emisorasGuardadas = new String[50];
+    private int emisoraIndex = 0;
