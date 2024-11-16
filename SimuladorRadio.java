33e271efc88f39c81d279c442db8f9a05cdce9f8
HipWilson <pen24760@uvg.edu.gt>
2024-11-15 18:35:46 -0600

    Inicio del menu

--- SimuladorRadio.java
+++ SimuladorRadio.java
@@ -5,6 +5,26 @@ public class SimuladorRadio {
         Scanner scanner = new Scanner(System.in);
         int opcion;
 
+        do {
+            System.out.println("\n--- Menu del Radio ---");
+            System.out.println("1. Encender radio");
+            System.out.println("2. Apagar radio");
+            System.out.println("3. Cambiar volumen");
+            System.out.println("4. Cambiar banda (AM/FM)");
+            System.out.println("5. Cambiar emisora");
+            System.out.println("6. Guardar emisora");
+            System.out.println("7. Cargar emisora");
+            System.out.println("8. Conectar telefono");
+            System.out.println("9. Desconectar telefono");
+            System.out.println("10. Cambiar a llamada en espera");
+            System.out.println("11. Ver pronostico del tiempo");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); 
+
+            String resultado = ""; // Iniciar el mensaje
+        }
     }
 
 }
