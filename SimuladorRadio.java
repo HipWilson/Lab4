b9c69a49e32155cf3647b0366554b37a8d07b26d
HipWilson <pen24760@uvg.edu.gt>
2024-11-15 18:34:30 -0600

    Inicio de creacion del main

---
+++ SimuladorRadio.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SimuladorRadio {
+    public static void main(String[] args) {
+        RadioClaseC radio = new RadioClaseC();
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+    }
+
+}
