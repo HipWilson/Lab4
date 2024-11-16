0b4c24e8754770f66c2c88322bf9c03f8a6feb2a
HipWilson <pen24760@uvg.edu.gt>
2024-11-15 18:31:19 -0600

    Metodos Funcionales
    Creacion de todos los metodos

---
+++ InterfaceGeneral.java
@@ -0,0 +1,11 @@
+public interface InterfaceGeneral {
+    String encender();
+    String apagar();
+    String cambiarVolumen(int incremento);
+    String cambiarBanda();
+    String cambiarEmisora(double incremento);
+    String guardarEmisora(String emisora);
+    String cargarEmisora(int posicion);
+    String conectarTelefono();
+    String desconectarTelefono();
+}
