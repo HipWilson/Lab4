4322f9bbf8598f18d5032b747fb1bc5ad91d7641
JCova10 <ova24701@uvg.edu.gt>
2024-11-15 19:37:56 -0800

    Update InterfaceClaseC.java
    parte2

--- InterfaceClaseC.java
+++ InterfaceClaseC.java
@@ -5,3 +5,10 @@ public class RadioClaseC implements InterfaceGeneral, InterfaceClaseC {
     private double frecuencia = 88.1;
     private String[] emisorasGuardadas = new String[50];
     private int emisoraIndex = 0;
+
+    // Metodos de FuncionalidadesGenerales
+    @Override
+    public String encender() {
+        this.encendido = true;
+        return mostrarEstado("Radio encendido.");
+    }
